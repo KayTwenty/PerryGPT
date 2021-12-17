47644abc740423ec51679af5c64ce5deadcd182c
KayTwenty <davidanton037@gmail.com>
2021-12-17 10:42:39 -0500

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -73,7 +73,7 @@ else:
     prompt = re.sub(r'http\S+', '', new_tweet[1]).strip() # no image or video links
     t1 = "I'm Perry the Platabot, I can always predict your next move when playing any fighter games!"
     t2 = "Gamers should not think over playing video games, It's all the matter of perfection!"
-    t3 = "Platypuses are wholesome, soft, and caring when talking to them!"
+    t3 = "This is actually not a tweet, I expected something far more better!"
     prompt_updated = f"{prompt} Perry {t1} Perry {t2} Perry {t3} Perry I {prompt}" # magic sauce
     
     def process(s):
